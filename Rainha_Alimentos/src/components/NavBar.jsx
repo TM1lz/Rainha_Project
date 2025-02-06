55c0535a6b5139366949f818a8160836cb9d45b0
TM1lz <Alisson.1007697@gmail.com>
2025-02-06 18:16:47 -0300

    adicionei a img , logo no site e escolhi a cor principal

--- Rainha_Alimentos/src/components/NavBar.jsx
+++ Rainha_Alimentos/src/components/NavBar.jsx
@@ -1,10 +1,17 @@
 import { NavLink } from "react-router-dom";
+
+//Estilo Css
+
 import styler from "./NavBar.module.css"
+
+//Imagem da logo
+
+import logo from "../assets/logo.png"
 export default function NavBar() {
   return (
     <div className= { styler.container}>
-      <div className="logo-nav">
-        <h1>Logo</h1>
+      <div className={styler.logo_nav}>
+        <img className={styler.logo} src={logo}/>
       </div>
       <div className={styler.nav_menu}>
         <ul>
